8588271739b241a33e15b4e9fbf7b39df344e4ef
Brandon Payton <brandon@happycode.net>
2026-07-11 04:46:55 -0400

    nano: add the terminal editor formula

---
+++ Formula/nano.rb
@@ -0,0 +1,65 @@
+require_relative "../Kandelo/formula_support/kandelo_formula_support"
+
+class Nano < Formula
+  include KandeloFormulaSupport
+
+  desc "Small, friendly text editor for Kandelo"
+  homepage "https://www.nano-editor.org/"
+  url "https://ftpmirror.gnu.org/gnu/nano/nano-8.0.tar.xz"
+  mirror "https://www.nano-editor.org/dist/v8/nano-8.0.tar.xz"
+  sha256 "c17f43fc0e37336b33ee50a209c701d5beb808adc2d9f089ca831b40539c9ac4"
+  license "GPL-3.0-or-later"
+
+  depends_on "automattic/kandelo-homebrew/ncurses"
+
+  skip_clean "bin/nano"
+
+  def install
+    kandelo_require_arch!("wasm32")
+    ncurses = formula_opt_prefix("automattic/kandelo-homebrew/ncurses")
+    guest_prefix = "/home/linuxbrew/.linuxbrew"
+
+    kandelo_wasm_build do |root|
+      ENV["CFLAGS"] = "-O2 -gline-tables-only -fdebug-compilation-dir=."
+      ENV["NCURSESW_CFLAGS"] = "-I#{ncurses}/include"
+      ENV["NCURSESW_LIBS"] = "-L#{ncurses}/lib -lncursesw -ltinfow"
+
+      # The SDK site owns target facts; these are gnulib probes that cannot run
+      # while cross-compiling.
+      ENV["gl_cv_func_strerror_0_works"] = "yes"
+      ENV["gl_cv_func_working_getdelim"] = "yes"
+
+      system kandelo_configure, *kandelo_std_configure_args,
+        "--sysconfdir=#{guest_prefix}/etc",
+        "--disable-nls",
+        "--disable-browser",
+        "--disable-speller",
+        "--disable-libmagic",
+        "--enable-utf8",
+        "--disable-extra"
+
+      system "make", "-j#{ENV.make_jobs}"
+
+      instrumented = buildpath/"src/nano.instrumented"
+      system "#{root}/scripts/run-wasm-fork-instrument.sh", buildpath/"src/nano", "-o", instrumented
+      mv instrumented, buildpath/"src/nano"
+
+      system "make", "install"
+    end
+  end
+
+  test do
+    version_output = kandelo_run_wasm(bin/"nano", ["--version"])
+    assert_match(/^ GNU nano, version 8\.0$/, version_output.lines.first)
+
+    help_output = kandelo_run_wasm(bin/"nano", ["--help"])
+    assert_includes help_output, "--rcfile=<file>"
+    assert_includes help_output, "--ignorercfiles"
+    assert_includes help_output, "--modernbindings"
+
+    binary = File.binread(bin/"nano")
+    assert_includes binary, "/home/linuxbrew/.linuxbrew/etc/nanorc"
+    refute_includes binary, prefix.to_s
+    refute_match %r{/Users/[^/]+/}, binary
+  end
+end
